8b9715f9a2f5372b553d139f81b319bb51b38083
Arthur Wuterich <ArthurWut@gmail.com>
2019-11-29 20:40:07 -0800

    Cleanup

--- MiniatureTactics/entry.cpp
+++ MiniatureTactics/entry.cpp
@@ -1,6 +1,7 @@
 #include "src/game/GameApplication.h"
 
-int main(int argc, char* args[]) {
+int main(int argc, char* args[]) 
+{
 	std::make_shared<MTGame::GameApplication>()->run(argc, args);
 	return 0;
 }
