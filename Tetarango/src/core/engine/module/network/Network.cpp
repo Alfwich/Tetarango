c4c4887d50fb777525b3268bc88f7073472736fc
Arthur Wuterich <ArthurWut@gmail.com>
2020-01-14 18:04:44 -0800

    Network Cleanup

--- Tetarango/src/core/engine/module/network/Network.cpp
+++ Tetarango/src/core/engine/module/network/Network.cpp
@@ -9,9 +9,24 @@ namespace AW
 		this->thread = thread;
 	}
 
-	int Network::httpGet(std::string host, std::string path, std::weak_ptr<INetworkListener> listener)
+	void Network::processHostAndPath(std::string& host, std::string& path)
 	{
-		const auto bundle = std::make_shared<NetworkRequestBundle>(host, path, listener);
+		if (!path.empty() && path[0] != '/') path = '/' + path;
+	}
+
+	std::tuple<std::string, std::string> Network::breakUrlIntoHostAndPath(const std::string& url)
+	{
+		const auto delimitPos = url.find_first_of('/');
+		if (delimitPos != std::string::npos)
+		{
+			return std::make_tuple(url.substr(0, delimitPos), url.substr(delimitPos));
+		}
+
+		return std::make_tuple(url, "");
+	}
+
+	int Network::startRequest(const std::shared_ptr<NetworkRequestBundle>& bundle)
+	{
 		const auto id = thread->doWork(bundle.get(), [](void* data)
 			{
 				NetworkRequestBundle* bundle = (NetworkRequestBundle*)data;
@@ -26,26 +41,56 @@ namespace AW
 				return data;
 			}, weak_from_this());
 
+		Logger::instance()->log("Network::Started network request for host: " + bundle->host + ", path: " + bundle->path + ", on worker: " + std::to_string(id));
+
+		return id;
+	}
+
+	int Network::createBundleAndStartRequest(NetworkRequestMethod method, std::string host, std::string path, std::weak_ptr<INetworkListener> listener)
+	{
+		processHostAndPath(host, path);
+
+		const auto bundle = std::make_shared<NetworkRequestBundle>(NetworkRequestMethod::Get, host, path, listener);
+		const auto id = startRequest(bundle);
+
 		requestMap[id] = bundle;
 
 		return id;
 	}
 
-	void Network::onWorkDone(WORKER_ID workerId, WorkerTaskCode type, std::shared_ptr<AsyncResultBundle> result)
+	void Network::endRequest(const WORKER_ID id)
 	{
-		if (requestMap.count(workerId) == 0)
+		if (requestMap.count(id) == 0)
 		{
-			Logger::instance()->logCritical("Network::Got threading callback without associated worker listing");
+			Logger::instance()->logCritical("Network::Attempted to end request without associated worker");
 			return;
 		}
 
-		const auto bundle = requestMap[workerId];
-		requestMap.erase(workerId);
+		const auto bundle = requestMap[id];
+		requestMap.erase(id);
+
+		Logger::instance()->log("Network::Ended network request for worker: " + std::to_string(id) + ", status: " + std::to_string(bundle->status));
 
 		const auto listener = bundle->listener.lock();
 		if (listener != nullptr)
 		{
 			listener->onNetworkCallCompleted(bundle->status, bundle->body);
 		}
+	}
+
+	int Network::httpGet(std::string host, std::string path, std::weak_ptr<INetworkListener> listener)
+	{
+		return createBundleAndStartRequest(NetworkRequestMethod::Get, host, path, listener);
+	}
+
+	int Network::httpGet(std::string url, std::weak_ptr<INetworkListener> listener)
+	{
+		const auto hostAndPath = breakUrlIntoHostAndPath(url);
+		return httpGet(std::get<0>(hostAndPath), std::get<1>(hostAndPath), listener);
+	}
+
+	void Network::onWorkDone(WORKER_ID workerId, WorkerTaskCode type, std::shared_ptr<AsyncResultBundle> result)
+	{
+		endRequest(workerId);
 	}
 }
--- Tetarango/src/core/engine/module/network/Network.h
+++ Tetarango/src/core/engine/module/network/Network.h
@@ -7,11 +7,21 @@
 
 namespace AW
 {
+	enum class NetworkRequestMethod
+	{
+		Get,
+		Post
+	};
+
 	class NetworkRequestBundle
 	{
 	public:
-		NetworkRequestBundle(std::string host, std::string path, std::weak_ptr<INetworkListener> listener) 
-			: host(host), path(path), listener(listener) {}
+		NetworkRequestBundle(NetworkRequestMethod method, std::string host, std::string path, std::weak_ptr<INetworkListener> listener)
+			: method(method), host(host), path(path), listener(listener)
+		{
+		}
+
+		const NetworkRequestMethod method;
 		const std::string host, path;
 		const std::weak_ptr<INetworkListener> listener;
 
@@ -23,14 +33,21 @@ namespace AW
 	{
 		std::shared_ptr<Thread> thread;
 
+		int requestId = 0;
 		std::unordered_map<WORKER_ID, std::shared_ptr<NetworkRequestBundle>> requestMap;
 
-		int requestId = 0;
+		void processHostAndPath(std::string& host, std::string& path);
+		std::tuple<std::string, std::string> breakUrlIntoHostAndPath(const std::string& url);
+
+		int startRequest(const std::shared_ptr<NetworkRequestBundle>& bundle);
+		int createBundleAndStartRequest(NetworkRequestMethod method, std::string host, std::string path, std::weak_ptr<INetworkListener> listener);
+		void endRequest(const WORKER_ID id);
 
 	public:
 		void bindThread(std::shared_ptr<Thread> thread);
 
 		int httpGet(std::string host, std::string path, std::weak_ptr<INetworkListener> listener);
+		int httpGet(std::string url, std::weak_ptr<INetworkListener> listener);
 
 		virtual void onWorkDone(WORKER_ID workerId, WorkerTaskCode type, std::shared_ptr<AsyncResultBundle> result) override;
 	};
