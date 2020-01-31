88981a34c03ff02f1f23132a3a6a700058d415ef
Arthur Wuterich <ArthurWut@gmail.com>
2020-01-31 08:43:47 -0800

    Add Lua Context Abstraction

--- Tetarango/src/core/engine/module/lua/Lua.cpp
+++ Tetarango/src/core/engine/module/lua/Lua.cpp
@@ -1,10 +1,8 @@
 #include "Lua.h"
 
-#include "lua.hpp"
-
 namespace
 {
-	lua_State* L;
+	std::string dummyCode = "";
 }
 
 namespace AW
@@ -14,37 +12,46 @@ namespace AW
 		this->asset = asset;
 	}
 
-	std::string& Lua::loadScriptForPath(const std::string& path)
+	std::string& Lua::loadScriptForPath(const std::string& path, bool allowCached)
 	{
-		if (scripts.count(path) == 0)
+		if (!allowCached || fileScriptCache.count(path) == 0)
 		{
 			const auto bundle = asset->getAssetBundle(path);
 			if (bundle != nullptr)
 			{
-				scripts[path] = std::string(bundle->data.get(), bundle->size);
+				fileScriptCache[path] = std::string(bundle->data.get(), bundle->size);
 			}
 		}
 
-		return scripts.at(path);
+		if (fileScriptCache.count(path) == 0)
+		{
+			Logger::instance()->logCritical("Lua::Failed to load code at path:" + path + ", this does not exist");
+			return dummyCode;
+		}
+
+		return fileScriptCache.at(path);
 	}
 
-	void Lua::onInit()
+	void Lua::executeLua(const std::string& script)
 	{
-		L = luaL_newstate();
-		executeLuaScript("res/lua/test.lua");
-	}
+		if (isCleanedUp)
+		{
+			Logger::instance()->logCritical("Lua::Failed to run script, Lua module has been cleaned up\n" + script);
+			return;
+		}
 
-	void Lua::onCleanup()
-	{
-		lua_close(L);
-		L = nullptr;
-	}
+		if (currentActiveContextId == -1)
+		{
+			Logger::instance()->logCritical("Lua::Failed to run script, no active context available\n" + script);
+			return;
+		}
 
-	void Lua::executeLuaScript(std::string path)
-	{
-		if (isCleanedUp) return;
-
-		const auto script = loadScriptForPath(path);
+		const auto L = getCurrentContextLuaState();
+		if (L == nullptr)
+		{
+			Logger::instance()->logCritical("Lua::Failed to run script, context could not be acquired\n" + script);
+			return;
+		}
 
 		if (luaL_dostring(L, script.c_str()) == LUA_OK)
 		{
@@ -59,5 +66,112 @@ namespace AW
 			std::string err = lua_tostring(L, -1);
 			Logger::instance()->logCritical("Lua::Error running script: " + err);
 		}
+	}
+
+	void Lua::createDefaultContext()
+	{
+		if (defaultContext == -1 && !isCleanedUp)
+		{
+			defaultContext = createNewContext();
+			setActiveContextId(defaultContext);
+		}
+	}
+
+	void Lua::cleanupDefaultContext()
+	{
+		if (defaultContext != -1)
+		{
+			lua_close(contexts.at(defaultContext));
+			defaultContext = -1;
+		}
+	}
+
+	void Lua::cleanupUserCreatedContexts()
+	{
+		std::vector<int> ids;
+		for (const auto idsToContexts : contexts) ids.emplace_back(idsToContexts.first);
+		for (const auto id : ids)
+		{
+			if (id != defaultContext)
+				cleanupContext(id);
+		}
+	}
+
+	lua_State* Lua::getCurrentContextLuaState()
+	{
+		return (contexts.count(currentActiveContextId) != 0) ? contexts.at(currentActiveContextId) : nullptr;
+	}
+
+	int Lua::createNewContext()
+	{
+		const auto id = nextInactiveContextId++;
+		const auto L = luaL_newstate();
+		contexts[id] = L;
+		return id;
+	}
+
+	void Lua::setActiveContextId(int id)
+	{
+		if (id == -1)
+		{
+			currentActiveContextId = defaultContext;
+			return;
+		}
+
+		if (contexts.count(id) == 0)
+		{
+			Logger::instance()->logCritical("Lua::Failed to change to context id:" + std::to_string(id) + ", as this context does not exist!");
+			currentActiveContextId = defaultContext;
+			return;
+		}
+
+		currentActiveContextId = id;
+	}
+
+	void Lua::cleanupContext(int id)
+	{
+		if (contexts.count(id) == 0)
+		{
+			Logger::instance()->logCritical("Lua::Failed to cleanup context id:" + std::to_string(id) + ", no context exists for this id");
+			return;
+		}
+
+		if (id == defaultContext)
+		{
+			Logger::instance()->logCritical("Lua::Failed to cleanup context id:" + std::to_string(id) + ", as this is the default context");
+			return;
+		}
+
+		lua_close(contexts.at(id));
+		contexts.erase(id);
+
+		if (currentActiveContextId == id)
+		{
+			setActiveContextId(defaultContext);
+		}
+	}
+
+	void Lua::executeLuaScript(std::string path, bool allowCached)
+	{
+		const auto script = loadScriptForPath(path, allowCached);
+		executeLuaString(script);
+	}
+
+	void Lua::executeLuaString(const std::string& script)
+	{
+		executeLua(script);
+	}
+
+	void Lua::onInit()
+	{
+		createDefaultContext();
+		executeLuaScript("res/lua/test.lua");
+	}
+
+	void Lua::onCleanup()
+	{
+		cleanupUserCreatedContexts();
+		cleanupDefaultContext();
+		contexts.clear();
 	}
 }
--- Tetarango/src/core/engine/module/lua/Lua.h
+++ Tetarango/src/core/engine/module/lua/Lua.h
@@ -4,15 +4,27 @@
 #include "engine/module/asset/Asset.h"
 #include "engine/module/asset/ResourceBundle.h"
 
+#include "lua.hpp"
+
 namespace AW
 {
 	class Lua : public IBaseModule
 	{
 		std::shared_ptr<Asset> asset;
+		int nextInactiveContextId = 0, currentActiveContextId = -1, defaultContext = -1;
 
-		std::unordered_map<std::string, std::string> scripts;
+		std::unordered_map<std::string, std::string> fileScriptCache;
+		std::unordered_map<int, lua_State*> contexts;
 
-		std::string& loadScriptForPath(const std::string& path);
+		std::string& loadScriptForPath(const std::string& path, bool allowCached = true);
+
+		void executeLua(const std::string& script);
+		void createDefaultContext();
+
+		void cleanupDefaultContext();
+		void cleanupUserCreatedContexts();
+
+		lua_State* getCurrentContextLuaState();
 
 	public:
 		void bindAsset(std::shared_ptr<Asset> asset);
@@ -20,6 +32,11 @@ namespace AW
 		void onInit() override;
 		void onCleanup() override;
 
-		void executeLuaScript(std::string path);
+		int createNewContext();
+		void setActiveContextId(int id);
+		void cleanupContext(int id);
+
+		void executeLuaScript(std::string path, bool allowCached = true);
+		void executeLuaString(const std::string& script);
 	};
 }
